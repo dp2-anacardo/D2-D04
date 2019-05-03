2288879a8f2783c934a5c36f1ba8b5958fb7023a
ManuErCaximba <ercaximbero96@gmail.com>
2019-05-03 14:13:38 +0200

    commit para quitar una cosa

--- src/main/java/repositories/AdministratorRepository.java
+++ src/main/java/repositories/AdministratorRepository.java
@@ -96,4 +96,6 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 
     @Query("select (count(f1)*100.0)/(select count(f2) from Finder f2) from Finder f1 where f1.positions is empty")
     Double getRatioOfEmptyFinders();
+
+    //@Query("select avg(1.0*(select ))");
 }
